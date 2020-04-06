6df4463ab2394c3fd5b6df1f804f6448667ade60
Anurudra.Goel <Anurudra.Goel@LAP-TZD-0X73717.IN.NIIT-Tech.com>
2020-04-06 15:40:23 +0530

    push

--- myTestProj/src/main/java/com/service/MyService.java
+++ myTestProj/src/main/java/com/service/MyService.java
@@ -7,19 +7,13 @@ import com.myPojo.Person;
 
 public class MyService {
 
-	public static List<Person>  getAddress() {
+	public static List<String>  getAddress() {
 
-		List<Person> lis = new ArrayList<Person>();
+		List<String> lis = new ArrayList<String>();
 
-		Person person = new Person();
-		person.setAge("37");
-		person.setName("Anurudra Goel");
-		Person person1 = new Person();
-		person.setAge("34");
-		person.setName("Neha Goel");
-
-		lis.add(person);
-		lis.add(person1);
+		
+		lis.add("Anurudra");
+		lis.add("Neha Agrawal");
 
 		return lis;
 
