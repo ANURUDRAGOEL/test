58eb417dc488faa20c03268906f21ee71e972494
ANURUDRAGOEL <anurudragoel@gmail.com>
2021-08-03 15:32:57 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,4 +2,4 @@ $param1=$args[0]
 write-host $param1 
 Write-Host "Hello, World!"
 New-Item -Path . -Name "testfile1.txt" -ItemType "file" -Value "This is a text string."
-compress-archive -path $param1+"\" -destinationpath $param1+"\logs.zip" -update -compressionlevel optimal
+compress-archive -path "$param1" -destinationpath "$param1\logs.zip" -update -compressionlevel optimal
