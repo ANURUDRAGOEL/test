e60eb784e7b4e024e1ba9f6b5dceeaf63f3af6e5
ANURUDRAGOEL <anurudragoel@gmail.com>
2021-08-03 15:07:49 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,3 @@
 Write-Host "Hello, World!"
+New-Item -Path  "$(Build.SourcesDirectory)\testfile1.txt" -ItemType "file" -Value "This is a text string."
+compress-archive -path "$(Build.SourcesDirectory)" -destinationpath "$(Build.SourcesDirectory)\logs.zip" -update -compressionlevel optimal
